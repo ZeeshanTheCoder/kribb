18b65c80d7224eaeb482c6d05fcea3008e4fb31d
Muhammad Zeeshan <ZeeshanZahid663@gmail.com>
2026-09-19 11:43:51 +0500

    first commit

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,30 @@
+import { useAuth } from "@clerk/expo";
+import { useRouter } from "expo-router";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Profile() {
+
+  const { signOut } = useAuth();
+
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      router.replace("/sign-in");
+    } catch (error) {
+      console.log("Error Sign Out", error)
+    }
+  }
+
+  return (
+    <SafeAreaView>
+      <Text>Profile</Text>
+      <TouchableOpacity onPress={handleSignOut} >
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
---
+++ app/(root)/(tabs)/search.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Search() {
+  return (
+    <SafeAreaView className="flex-1 bg-gray-50">
+      <View>
+        <Text>Search Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
